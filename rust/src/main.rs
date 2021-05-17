e986c968297d02136d165e23b8cc87d83947515c
刘建东 <jdlau@qq.com>
2021-05-17 05:59:03 +0000

    rust: 添加cli参数、子命令解析等功能，以及文件打开和读取内容

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,14 @@
 #![allow(unused)]
 
 use std::fmt::Debug;
+use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::ops::Add;
+use std::path::PathBuf;
+use std::str;
+
+use structopt::StructOpt;
 
 mod usersrv; // 还要在这里声明mod的文件路径，好难理解
 use usersrv::user::User; // 文件路径::模块名::结构体名
@@ -34,6 +41,91 @@ fn main() {
 
     let r3 = u.resi();
     println!("r3: {:?}", r3.unwrap());
+
+    let opt = Opt::from_args();
+    println!("{:#?}", opt);
+
+    match opt {
+        Opt::Add { debug, verbose } => {
+            println!("{:#?}, {:#?}", debug, verbose)
+        }
+        Opt::Get {
+            speed,
+            output,
+            nb_cars,
+            level,
+            files,
+        } => {
+            println!(
+                "{:#?}, {:#?}, {:#?}, {:#?}, {:#?}",
+                speed, output, nb_cars, level, files
+            );
+            for file in files {
+                println!("{:#?}", file);
+                let fr = File::open(file);
+                match fr {
+                    Ok(mut f) => {
+                        println!("file: {:#?}", f);
+                        let data = f.metadata();
+                        println!("data: {:#?}", data);
+
+                        let mut buf = Vec::new();
+                        f.read_to_end(&mut buf);
+                        println!("buf: {:#?}", str::from_utf8(&buf))
+                    }
+                    Err(e) => {
+                        println!("e: {:#?}", e)
+                    }
+                };
+            }
+        }
+        _ => (),
+    };
+}
+
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "rcli", about = "rust cli demo")]
+enum Opt {
+    #[structopt(name = "add")]
+    Add {
+        // A flag, true if used in the command line. Note doc comment will
+        // be used for the help message of the flag. The name of the
+        // argument will be, by default, based on the name of the field.
+        /// Activate debug mode
+        #[structopt(short, long)]
+        debug: bool,
+
+        // The number of occurrences of the `v/verbose` flag
+        /// Verbose mode (-v, -vv, -vvv, etc.)
+        #[structopt(short, long, parse(from_occurrences))]
+        verbose: u8,
+    },
+
+    #[structopt(name = "get")]
+    Get {
+        /// Set speed
+        #[structopt(short, long, default_value = "42")]
+        speed: f64,
+
+        /// Output file
+        #[structopt(short, long, parse(from_os_str))]
+        output: PathBuf,
+
+        // the long option will be translated by default to kebab case,
+        // i.e. `--nb-cars`.
+        /// Number of cars
+        #[structopt(short = "c", long)]
+        nb_cars: Option<i32>,
+
+        /// admin_level to consider
+        #[structopt(short, long)]
+        level: Vec<String>,
+
+        /// Files to process
+        #[structopt(name = "FILE", parse(from_os_str))]
+        files: Vec<PathBuf>,
+    },
 }
 
 fn add<T>(a: T, b: T) -> T
